c0c9972d7932c2ddcc17221e27e36329a76859a6
Alexander Anpleenko <vaeum@yahoo.com>
2017-04-23 00:26:36 +0300

    add bootstrap

--- tasks/style.js
+++ tasks/style.js
@@ -4,7 +4,6 @@ import { config, $, bs, errorHandler, isDev } from './config';
 const reload = bs.reload;
 
 const source = [
-  './node_modules/bootstrap-only-css/dist/bootstrap.css',
   './node_modules/css.modifiers/dist/modifiers.css',
   config.src.style,
 ];
