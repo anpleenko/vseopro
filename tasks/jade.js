1da84b95d244efac1c10c6c503a0b00d411bfa8d
Alexander Anpleenko <vaeum@yahoo.com>
2017-04-24 12:18:40 +0300

    fix random

--- tasks/jade.js
+++ tasks/jade.js
@@ -3,32 +3,29 @@ import gulp from 'gulp';
 import Chance from 'chance';
 import moment from 'moment';
 import data from '../assets/data';
+import faker from 'faker';
 import { config, $, bs, errorHandler, isDev, isProd } from './config';
-
-//Если с английского на русский, то передаём вторым параметром true.
-var transliterate = (
-  function() {
-    var
-      rus = "щ   ш  ч  ц  ю  я  ё  ж  ъ  ы  э  а б в г д е з и й к л м н о п р с т у ф х ь".split(/ +/g),
-      eng = "shh sh ch cz yu ya yo zh `` y' e` a b v g d e z i j k l m n o p r s t u f x `".split(/ +/g)
-    ;
-    return function(text, engToRus) {
-      var x;
-      for(x = 0; x < rus.length; x++) {
-        text = text.split(engToRus ? eng[x] : rus[x]).join(engToRus ? rus[x] : eng[x]);
-        text = text.split(engToRus ? eng[x].toUpperCase() : rus[x].toUpperCase()).join(engToRus ? rus[x].toUpperCase() : eng[x].toUpperCase());
-      }
-      return text;
-    }
-  }
-)();
 
 const chance = new Chance();
 moment.locale('ru');
+
+// Если с английского на русский, то передаём вторым параметром true.
+const transliterate = (text, engToRus) => {
+  const rus = 'щ   ш  ч  ц  ю  я  ё  ж  ъ  ы  э  а б в г д е з и й к л м н о п р с т у ф х ь'.split(/ +/g);
+  const eng = "shh sh ch cz yu ya yo zh `` y' e` a b v g d e z i j k l m n o p r s t u f x `".split(/ +/g);
+
+  rus.map((e, x) => {
+    text = text.split(engToRus ? eng[x] : rus[x]).join(engToRus ? rus[x] : eng[x]);
+    text = text.split(engToRus ? eng[x].toUpperCase() : rus[x].toUpperCase()).join(engToRus ? rus[x].toUpperCase() : eng[x].toUpperCase());
+  });
+
+  return text;
+};
+
 const capitalizeFirstChar = string => string.charAt(0).toUpperCase() + string.substring(1);
 
 function getRandomText() {
-  return transliterate(chance.sentence({ words: 50 }), true);
+  return transliterate(faker.lorem.paragraph(), true);
 }
 
 function getWord() {
